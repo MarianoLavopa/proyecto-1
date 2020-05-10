350fc7895e421acb91552d68e109ac1c33b7b0de
MarianoLavopa <55409497+MarianoLavopa@users.noreply.github.com>
2020-05-09 21:04:43 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,190 @@
 
+var rotate_delay = 3000; // delay in milliseconds (5000 = 5 secs)
+current = 0;
+var maravillas=[
+"El Coliseo o Anfiteatro Flavio es un anfiteatro de la época del Imperio romano, construido en el siglo I y ubicado en el centro de la ciudad de Roma. Su denominación original, Anfiteatro Flavio, hace referencia a la dinastía Flavia de emperadores que lo construyó; su nombre posterior,Coliseo, y por el que es más conocido en la actualidad, se debe a una gran estatua que había cerca, el Coloso de Nerón, que no ha llegado hasta nosotros. Por su conservación e historia, el Coliseo es uno de los monumentosmás famosos de la Antigüedad clásica. Fue declarado Patrimonio de la Humanidad en 1980 por la Unesco y una de Las Nuevas SieteMaravillas del Mundo Moderno el 7 de julio de 2007.Los materiales utilizados en la construcción de este son bloques de travertino, hormigón,madera, ladrillo, piedra (toba), mármol y estuco. En la antigüedad poseía un aforo para unos 65 000 espectadores, con ochenta filas de gradas.",
+"Petra (en griego antiguo, Πέτρα, en árabe, البتراء al-Batrā´) es un importante enclave arqueológico en Jordania, y la capital del antiguo reino nabateo, cuyos pobladores la llamaban Raqmu (en árabe nabateo, الرقيم).1​ El nombre de Petra proviene del griego πέτρα que significa piedra, y su nombre es perfectamente idóneo; no se trata de una ciudad construida con piedra sino, literalmente, excavada y esculpida en la piedra. El asentamiento de Petra se localiza en un valle angosto, al este del valle de la Aravá que se extiende desde el mar Muerto hasta el Golfo de Aqaba. Los restos más célebres de Petra son sin duda sus construcciones labradas en la misma roca del valle (hemispeos), en particular, los edificios conocidos como el Khazneh (el Tesoro) y el Deir (el Monasterio). Petra fue fundada en la antigüedad hacia el final de siglo VIII a. C. por los edomitas, fue ocupada en el siglo VI a. C.",
+"Chichen Itzá (en maya: Boca-del-pozo (chichén) de los brujos-de-aguo (Itzá))​ es uno de los principales sitios arqueológicos de la península de Yucatán, en México. Está ubicado en el municipio de Tinum, en el estado de Yucatán. Consistió en una ciudad2​ o un centro ceremonial, que pasó por diversas épocas constructivas e influencias de los distintos pueblos que la ocuparon y que la impulsaron desde su fundación. Vestigio importante y renombrado de la civilización maya, las edificaciones principales que ahí perduran corresponden al periodo denominado clásico tardío o postclásico temprano (800-1100 d. C.). El 7 de julio de 2007, el Templo de Kukulcán, ubicado en Chichén Itzá, fue reconocido como una de las nuevas siete maravillas del mundo moderno, por una iniciativa privada sin el apoyo de la Unesco, pero con el reconocimiento de millones de votantes alrededor del mundo.",
+"Machu Picchu (AFI: [ˌmɑtʃu ˈpiktʃu] en quechua, «Monte viejo») es el nombre contemporáneo que se da a una llaqta —antiguo poblado incaico andino— construida antes del siglo XV, ubicada en la Cordillera Oriental del sur de Perú, en la cadena montañosa de Los Andes a 2430 metros sobre el nivel del mar. Está ubicada en la región Cusco, provincia de Urubamba, distrito de Machupicchu, sobre el Valle Sagrado de los Incas, a 80 kilómetros al noroeste de la ciudad de Cusco, ciudad del Perú y por donde fluye el río Urubamba, que atraviesa la cordillera y se origina un cañón con clima de montaña tropical. Según muchos estudiosos su nombre original habría sido Llaqtapata. Según documentos de mediados del siglo XVI,1​ Machu Picchu habría sido una de las residencias de descanso de Pachacútec, noveno inca del Tahuantinsuyo entre 1438 y 1470.",
+"La Gran muralla China es una antigua fortificación china construida y reconstruida entre el siglo V a. C. y el siglo XVI (Edad Moderna) para proteger la frontera norte del Imperio chino durante las sucesivas dinastías imperiales de los ataques de los nómadas xiongnu de Mongolia y Manchuria. Contando sus ramificaciones y construcciones secundarias, se calcula que tiene sobre unos 21 200 kilómetros de largo,1​ desde la frontera con Corea, al borde del río Yalu, hasta el desierto de Gobi, a lo largo de un arco que delinea aproximadamente el borde sur de Mongolia Interior, aunque hoy solo se conserva un 30 % de ella.2​En promedio, mide de 6 a 7 metros de alto y de 4 a 5 metros de ancho. La muralla fue designada Patrimonio de la Humanidad por la Unesco en 1987. El 26 de enero de 2007 se dio a conocer que la muralla china fue elegida como una de las ganadoras en la lista de Las Nuevas Siete Maravillas del Mundo Moderno.",
+"El Taj Mahal (hindi: ताज महल Tāj Mahal, urdu: تاج محل Tāŷ Mahal 'Corona de los palacios'; /tɑːʒ mə'hɑl/) es un monumento funerario construido entre 1631 y 1654 en la ciudad de Agra, estado de Uttar Pradesh (India), a orillas del río Yamuna, por el emperador musulmán Shah Jahan de la dinastía mogol. El imponente conjunto de edificios se erigió en honor de su esposa favorita, Arjumand Banu Begum —más conocida como Mumtaz Mahal— que murió en el parto de su decimocuarto hijo. Se estima que su construcción necesitó el esfuerzo de unos 20 000 obreros bajo dirección de un conjunto de arquitectos liderados por el arquitecto de la corte, Ustad Ahmad Lahori. El Taj Mahal es considerado el más bello ejemplo de palacio, estilo que combina elementos de las arquitecturas islámica, persa,​india e incluso turca. Este monumento ha logrado especial notoriedad por el carácter romántico de su inspiración.",
+"El Cristo Redentor o Cristo del Corcovado es una enorme estatua de Jesús de Nazaret con los brazos abiertos mostrando a la ciudad de Río de Janeiro, Brasil. La estatua tiene una altura de 30,1 metros sobre un pedestal de 8 metros.1​ Este monumento fue inaugurado el 12 de octubre de 1931, después de aproximadamente cinco años de construcción. Considerada la estatua Art decó más grande del mundo, es reconocida como una de las nuevas siete maravillas del mundo moderno junto a Machu Picchu, la Gran Muralla China, el Coliseo de Roma, el Taj Mahal, Chichén Itzá y Petra. El Cristo del Corcovado, es uno de los atractivos turísticos más importantes de Brasil y, para los creyentes, un lugar de peregrinación. Al cumplirse los 80 años de su inauguración, en 2011, se celebró una exposición que repasa la historia del monumento y, sobre todo, cómo esta estatua se ha convertido en punto de referencia para los brasileños e inspiración para sus artistas."];
+
+function next() {
+    if (document.slideforma.slide[current+1]) {
+        document.images.show.src = document.slideforma.slide[current+1].value;
+        document.slideforma.slide.selectedIndex = ++current;
+        document.textform.innerHTML=maravillas[current];
+   }
+    else first();
+}
+
+function previous() {
+    if (current-1 >= 0) {
+        document.images.show.src = document.slideforma.slide[current-1].value;
+        document.slideforma.slide.selectedIndex = --current;
+        document.textform.innerHTML=maravillas[current];
+   }
+    else last();
+}
+
+function first() {
+    current = 0;
+    document.images.show.src = document.slideforma.slide[0].value;
+    document.slideforma.slide.selectedIndex = 0;
+    document.textform.innerHTML=maravillas[current];
+}
+
+function last() {
+    current = document.slideforma.slide.length-1;
+    document.images.show.src = document.slideforma.slide[current].value;
+    document.slideforma.slide.selectedIndex = current;
+    document.textform.innerHTML=maravillas[current];
+}
+
+function ap(text) {
+    document.slideforma.slidebutton.value = (text == "Stop") ? "AutoPlay" : "Stop";
+    rotate();
+    document.textform.innerHTML=maravillas[current];
+}
+
+function change() {
+    current = document.slideforma.slide.selectedIndex;
+    document.images.show.src = document.slideforma.slide[current].value;
+    document.textform.innerHTML=maravillas[current];
+}
+
+function rotate() {
+    if (document.slideforma.slidebutton.value == "Stop") {
+        current = (current == document.slideforma.slide.length-1) ? 0 : current+1;
+        document.images.show.src = document.slideforma.slide[current].value;
+        document.slideforma.slide.selectedIndex = current;
+        window.setTimeout("rotate()", rotate_delay);
+        document.textform.innerHTML=maravillas[current];
+   }
+}
+
+function seguridad_clave(clave){
+   var seguridad = 0;
+   if (clave.length!=0){
+      if (tiene_numeros(clave) && tiene_letras(clave)){
+         seguridad += 30;
+      }
+      if (tiene_minusculas(clave) && tiene_mayusculas(clave)){
+         seguridad += 30;
+      }
+      if (clave.length >= 4 && clave.length <= 5){
+         seguridad += 10;
+      }else{
+         if (clave.length >= 6 && clave.length <= 8){
+            seguridad += 30;
+         }else{
+            if (clave.length > 8){
+               seguridad += 40;
+            }
+         }
+      }
+   }
+   return seguridad            
+}  
+
+function muestra_seguridad_clave(clave,formulario){
+   seguridad=seguridad_clave(clave);
+   formulario.seguridad.value=seguridad + "%";
+}
+
+var numeros="0123456789";
+
+function tiene_numeros(texto){
+   for(i=0; i<texto.length; i++){
+      if (numeros.indexOf(texto.charAt(i),0)!=-1){
+         return 1;
+      }
+   }
+   return 0;
+}
+
+var letras="abcdefghyjklmnñopqrstuvwxyz";
+
+function tiene_letras(texto){
+   texto = texto.toLowerCase();
+   for(i=0; i<texto.length; i++){
+      if (letras.indexOf(texto.charAt(i),0)!=-1){
+         return 1;
+      }
+   }
+   return 0;
+}
+
+function tiene_minusculas(texto){
+   for(i=0; i<texto.length; i++){
+      if (letras.indexOf(texto.charAt(i),0)!=-1){
+         return 1;
+      }
+   }
+   return 0;
+}
+
+var letras_mayusculas="ABCDEFGHYJKLMNÑOPQRSTUVWXYZ";
+
+function tiene_mayusculas(texto){
+   for(i=0; i<texto.length; i++){
+      if (letras_mayusculas.indexOf(texto.charAt(i),0)!=-1){
+         return 1;
+      }
+   }
+   return 0;
+}
+
+var toggle = 0;
+myStorage = window.localStorage;
+
+function FUNCION(){
+	if(toggle == 0){
+        document.getElementById("style").href="css/app2.css";
+        myStorage.setItem("Style","css/app2.css");
+		toggle = toggle + 1;
+	}
+	else{
+        document.getElementById("style").href="css/app.css";
+        myStorage.setItem("Style","css/app.css");
+		toggle--;
+	}
+}
+
+CantEmailStorage = window.localStorage;
+
+emailStorage = window.localStorage;
+
+function loadState(){
+    var st = myStorage.getItem("Style");
+    if(st=="css/app2.css")
+        toggle++;
+    document.getElementById("style").href=st;
+    if(CantEmailStorage.getItem("elementos")==null)
+        CantEmailStorage.setItem("elementos",0);
+    for(i=0;i<5;i++){
+        var x = emailStorage.getItem(i);
+        document.getElementById(i).value=x;
+    }
+}
+
+function saving(){
+    var email = document.getElementById("email").value;
+    var elem = myStorage.getItem(email);
+    if(elem==null){
+        alert("Account Created Successfully");
+        myStorage.setItem(email,email);
+        var NumEmail = parseInt(CantEmailStorage.getItem("elementos"));
+        if (NumEmail==5){
+            CantEmailStorage.setItem("elementos",0);
+            NumEmail = 0;
+        }
+        emailStorage.setItem(NumEmail,email);
+        NumEmail++;
+        CantEmailStorage.setItem("elementos",NumEmail);
+    }
+    else{
+        alert("Sorry, the email is already used");
+    }
+}
